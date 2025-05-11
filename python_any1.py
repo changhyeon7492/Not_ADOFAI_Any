79086b164ec74f8157690760f5d8bce6ea7508ff
changhyeon7492 <101487242+changhyeon7492@users.noreply.github.com>
2025-05-11 22:32:49 +0900

    Update python_any1.py

--- python_any1.py
+++ python_any1.py
@@ -1,7 +1,10 @@
 # python_any1
 import random
 
-a = random.randint(1, 782893)
-b = str(input("아무 숫자나 입력하세요. ")
+a = random.randint(1, 78289368192)
+b = int(input("아무 숫자나 입력하세요: "))
 # 깃험에서 쓰는거 상당히 불편하네
+
+print(a, b)
+
 print(a**b)
