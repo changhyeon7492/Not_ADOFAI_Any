c0fe7736fdacf1ab148c9d9427d6346fd7ce2723
changhyeon7492 <101487242+changhyeon7492@users.noreply.github.com>
2025-05-11 22:51:21 +0900

    Update python_any2.py

--- python_any2.py
+++ python_any2.py
@@ -1,15 +1,32 @@
 # python_any2
-import discord
 
-bot = discord.Bot()
+a = open("python_any2.txt", "w")
+a.write("Typing Any")
+a.close()
 
-@bot.slash_command()
-async def hello(ctx, name: str = None):
-    name = name or ctx.author.name
-    await ctx.respond(f"Hello {name}!")
+# --------------------------------------
 
-@bot.user_command(name="Say Hello")
-async def hi(ctx, user):
-    await ctx.respond(f"{ctx.author.mention} says hello to {user.name}!")
+import random
 
-bot.run() # 토큰을 올린 순 없습니다..?
+d = str(random.randint(1,77919826982982) / random.randrange(1, 76))
+
+t = open("python_any2_2.txt", "w")
+t.write("The Password is " + d + ".")
+t.close()
+
+i = 0
+
+while True:
+    i = input("숫자를 입력하세요: ")
+    if i == d:
+        print("파일을 확인하셨군요.")
+        break
+    else:
+        print("잘못 입력하셨습니다. 혹시 파일이 생겼다던지 그렇진 않은가요?")
+
+# ----------------------------------------
+
+import os
+
+os.remove("python_any2.txt")
+os.remove("python_any2_2.txt")
