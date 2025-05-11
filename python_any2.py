0fde3637cf3a0f260fceca123fae38d6c5a9e208
changhyeon7492 <101487242+changhyeon7492@users.noreply.github.com>
2025-05-11 22:37:22 +0900

    Create python_any2.py

---
+++ python_any2.py
@@ -0,0 +1,15 @@
+# python_any2
+import discord
+
+bot = discord.Bot()
+
+@bot.slash_command()
+async def hello(ctx, name: str = None):
+    name = name or ctx.author.name
+    await ctx.respond(f"Hello {name}!")
+
+@bot.user_command(name="Say Hello")
+async def hi(ctx, user):
+    await ctx.respond(f"{ctx.author.mention} says hello to {user.name}!")
+
+bot.run() # 토큰을 올린 순 없습니다..?
